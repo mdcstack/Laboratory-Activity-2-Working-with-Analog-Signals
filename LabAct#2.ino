be98f87eec7154723da54ae0561af38699f31fa5
Matthew David L. Cartagena <matthewcartagena.mdcstack@gmail.com>
2025-10-05 12:05:34 +0800

    Update LabAct#2.ino

--- LabAct#2.ino
+++ LabAct#2.ino
@@ -1,32 +1,39 @@
 
-int pins[5]={12,11,10,9,8};
-int brightness[5] = {130,145,150,200,255}; 
+int pins[5] = { 12, 11, 10, 9, 8 };
 
-void setup(){ 
-  int i = 0;
-  while(i < 5){
-    pinMode(pins[i], OUTPUT);
-    i++;
-  }
+
+void setup() {
+	int i = 0;
+	while (i < 5) {
+		pinMode(pins[i], OUTPUT);
+		i++;
+	}
 }
 
- 
-void loop(){
- int on = 0;
- while (on < 5){
- 	analogWrite(pins[on], brightness[on]);
- 	delay(1000);
- 	on++;
- }
- 
- int off = 0;
- while (off < 5){
- 	analogWrite(pins[off], LOW);
- 	delay(1000);
- 	off++;
- }
-   
+void loop() {
+	int on = 0;
+
+
+	while (on < 5) {
+		int brightness = 0;
+		while (brightness <= 255) {
+			analogWrite(pins[on], brightness);
+			delay(10);
+			brightness++;
+		}
+		delay(1000);
+		on++;
+	}
+
+	int off = 0;
+	while (off < 5) {
+		int brightness = 255;
+		while (brightness >= 0) {
+			analogWrite(pins[off], brightness);
+			delay(10);
+			brightness--;
+		}
+		delay(1000);
+		off++;
+	}
 }
-          
-       
-    
